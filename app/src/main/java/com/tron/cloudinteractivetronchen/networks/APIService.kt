f3b4ec26d2b32ba6ecab991a46e8bae890202047
TronChen <dtp6284tj0@gmail.com>
2021-01-26 15:33:17 +0800

    Let Moshi change to Gson

--- app/src/main/java/com/tron/cloudinteractivetronchen/networks/APIService.kt
+++ app/src/main/java/com/tron/cloudinteractivetronchen/networks/APIService.kt
@@ -1,26 +1,15 @@
 package com.tron.cloudinteractivetronchen.networks
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.tron.cloudinteractivetronchen.BuildConfig
 import com.tron.cloudinteractivetronchen.data.Photo
-import kotlinx.coroutines.Deferred
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
 
 
 private const val BASE_URL = "https://jsonplaceholder.typicode.com"
-
-/**
- * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
- * full Kotlin compatibility.
- */
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
 
 private val client = OkHttpClient.Builder()
     .addInterceptor(HttpLoggingInterceptor().apply {
@@ -36,15 +25,12 @@ private val client = OkHttpClient.Builder()
  * object.
  */
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addConverterFactory(GsonConverterFactory.create())
     .baseUrl(BASE_URL)
     .client(client)
     .build()
 
-/**
- * A public interface that exposes the [getMarketingHots], [getProductList], [getUserProfile],
- * [userSignIn], [checkoutOrder] methods
- */
+
 interface CloudApiService {
 
     @GET("photos")
